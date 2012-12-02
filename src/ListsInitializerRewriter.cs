2dcafd83abd055dcb1dd376229102d4bca20b329
ntr <trushn@gmail.com>
2012-12-02 17:22:05 +0300

    Initial commit;  includes c# code compiler with short lists initialization

---
+++ src/ListsInitializerRewriter.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Roslyn.Compilers.CSharp;
+
+namespace RoslynCSharpExtensions
+{
+    /// <summary>
+    /// Adds possibility to initialize lists like "[<1;2;3>]" which will be short equivalent of "new List<int>{1;2;3}"
+    /// </summary>
+    public class ListsInitializerRewriter : SyntaxRewriter
+    {
+        private readonly SemanticModel semanticModel;
+
+        public ListsInitializerRewriter(SemanticModel semanticModel)
+        {
+            this.semanticModel = semanticModel;
+        }
+
+        public override SyntaxNode VisitElementAccessExpression(ElementAccessExpressionSyntax node)
+        {
+            var elements = GetListCollectionInitializerElements(node);
+            if (elements != null && elements.Count > 0)
+            {
+                var type = GetArgumentType(elements[0]);
+                var syntaxList = new SeparatedSyntaxList<ExpressionSyntax>();
+                var intializerExpr = Syntax.InitializerExpression(SyntaxKind.CollectionInitializerExpression, syntaxList.Add(elements.ToArray()));
+
+                var expr = Syntax.ParseExpression(string.Format("new List<{1}>{0}", intializerExpr, type));
+                return expr;
+            }
+            return base.VisitElementAccessExpression(node);
+        }
+
+        private static List<ExpressionSyntax> GetListCollectionInitializerElements(ElementAccessExpressionSyntax node)
+        {
+            var arguments = node.ArgumentList.Arguments;
+
+            if (arguments.Count == 0)
+                return null;
+            if (arguments.Count == 1)
+            {
+                var arg = arguments[0];
+                //should be greaterThen expression containing lessthen expression
+                var greaterThenBinaryExpression = arg.Expression as BinaryExpressionSyntax;
+                if (greaterThenBinaryExpression == null || greaterThenBinaryExpression.OperatorToken.Kind != SyntaxKind.GreaterThanToken)
+                    return null;
+                var lessThenBinaryExpression = greaterThenBinaryExpression.ChildNodes().OfType<BinaryExpressionSyntax>().FirstOrDefault();
+                if (lessThenBinaryExpression == null || lessThenBinaryExpression.OperatorToken.Kind != SyntaxKind.LessThanToken)
+                    return null;
+                var result = lessThenBinaryExpression.ChildNodes().OfType<ExpressionSyntax>().Single(child => !child.IsMissing);
+                return new List<ExpressionSyntax> { result };
+            }
+            else
+            {
+                var first = arguments[0].Expression as BinaryExpressionSyntax;
+                var last = arguments.Last().Expression as BinaryExpressionSyntax;
+                if (first == null || first.Kind != SyntaxKind.LessThanExpression
+                    || last == null || last.Kind != SyntaxKind.GreaterThanExpression)
+                {
+                    return null;
+                }
+
+                var result = new List<ExpressionSyntax> { first.Right, last.Left };
+                var totalArgs = arguments.Count;
+                result.InsertRange(1, arguments.Skip(1).Take(totalArgs - 2).Select(arg => arg.Expression));
+                return result;
+            }
+        }
+
+        private TypeSymbol GetArgumentType(ExpressionSyntax expression)
+        {
+            var info = semanticModel.GetTypeInfo(expression);
+
+            var resultantType = info.Type;
+            return resultantType;
+        }
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Roslyn.Compilers;
+using Roslyn.Compilers.CSharp;
+using Roslyn.Services;
+using Roslyn.Services.Formatting;
+
+namespace RoslynCSharpExtensions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var syntaxTree = SyntaxTree.ParseFile("code.txt");
+            var root = syntaxTree.GetRoot();
+
+            var newRoot = (CompilationUnitSyntax)(new ListsInitializerRewriter(GetSemanticsModel(syntaxTree)).Visit(root));
+            newRoot.Format(new FormattingOptions(false, 4, 4));
+
+            BuildExe(SyntaxTree.Create(newRoot));
+        }
+
+        static void BuildExe(SyntaxTree tree)
+        {
+            var result = GetCompilation(tree).Emit("test.exe");
+            Console.WriteLine("built into test.exe with success: {0}", result.Success);
+        }
+
+        private static SemanticModel GetSemanticsModel(SyntaxTree tree)
+        {
+            return GetCompilation(tree).GetSemanticModel(tree);
+        }
+
+        private static Compilation GetCompilation(SyntaxTree tree)
+        {
+            var mscorlib = MetadataReference.CreateAssemblyReference("mscorlib");
+
+            var compilation = Compilation.Create(
+                outputName: "HelloWorld",
+                syntaxTrees: new[] { tree },
+                references: new[] { mscorlib });
+            return compilation;
+        }
+    }
+}
